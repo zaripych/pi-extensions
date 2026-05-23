99d9bf7b8ca8996c73a18d584c492f2689a3d60d
Rinat Zaripov <zaripov.rinat@gmail.com>
2026-05-23 23:24:54 +1000

    chore: migrate to pnpm and add dev tooling

---
+++ .prettierrc.mjs
@@ -0,0 +1,2 @@
+import { default as config } from './foundation/.prettierrc.mjs'
+export default config
---
+++ lint-staged.config.mjs
@@ -0,0 +1,2 @@
+import { default as config } from './foundation/lint-staged.config.mjs'
+export default config
